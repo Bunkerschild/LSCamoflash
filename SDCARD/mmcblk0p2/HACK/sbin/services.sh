81f8cd9d34dd5508ce4de5ccc64ef6ce5dc92c67
Bunkerschild <bunkerschild@welter.rocks>
2025-02-20 17:14:07 +0100

    Update services.sh

--- SDCARD/mmcblk0p2/HACK/sbin/services.sh
+++ SDCARD/mmcblk0p2/HACK/sbin/services.sh
@@ -94,8 +94,7 @@ fi
 if [ "$crond_enabled" = "1" ]; then
 	has_crond=`pgrep -f "crond"`
  	if [ "$has_crond" = "" ]; then
-  		[ -f "$sd_crontabs/root"] || touch $sd_crontabs/root
-    		crond -L $crond_log -c $sd_crontabs -b
+    		$sd_bin/busybox crond -L $crond_log -c $sd_crontabs -b
   	fi
 fi
 
